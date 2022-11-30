8dbae7f7d01884dd050e6c978010156bf955c12c
Ali Fasihy <fasihy.ali@gmail.com>
2022-11-30 18:50:45 -0500

    finish code

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,9 +10,10 @@ export default function NavBar({ user, setUser }) {
 
   return (
     <nav>
-      <Link to="/orders">Order History</Link>
+      
+      <Link to="/orders">New Post</Link>
       &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
+      <Link to="/orders/new"></Link>
       &nbsp; &nbsp; <span className="name">Hey there {user.name}!</span>
       &nbsp; &nbsp; <Link to="" onClick={handleLogOut}>Log Out</Link>
 
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,8 +4,9 @@ import { Routes, Route } from 'react-router-dom';
 
 // Components
 import AuthPage from '../AuthPage/AuthPage';
-import NewOrderPage from '../NewOrderPage/NewOrderPage';
-import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import NewPost from '../CreateNewPost/CreateNewPost';
+
+import PostHistory from '../DisplayAllPosts/DisplayAllPosts';
 import NavBar from '../../components/NavBar/NavBar';
 
 // Helpers
@@ -21,10 +22,12 @@ export default function App() {
       { user ?
           <>
           <NavBar user={user} setUser={setUser} />
+          
             <Routes>
               {/* Route components in here */}
-              <Route path="/orders/new" element={<NewOrderPage />} />
-              <Route path="/orders" element={<OrderHistoryPage />} />
+              <Route path="/orders/new" element={<NewPost />} />
+              <Route path="/orders" element={<PostHistory />} />
+              
             </Routes>
           </>
           :
